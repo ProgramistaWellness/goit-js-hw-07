0d6812a4dfb5e6679275145d106d3228d325af9e
Oskar Przybysz <wellnessinstructor.oskar@gmail.com>
2023-05-12 07:28:17 +0200

    Started solving js-hw-07

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,11 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    (image) =>
+      `<li class="gallery__item"><img class="gallery__image" src="${image.preview}" alt="${image.description}" width="719" height="480"/></li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", markup);
 console.log(galleryItems);
