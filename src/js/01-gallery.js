5f26ca863159e9e3f34ccf23b671da68b55da08d
Oskar Przybysz <117311648+ProgramistaWellness@users.noreply.github.com>
2023-05-16 21:28:39 +0200

    Added gallery and event listener

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    (image) =>
+      `<li class="gallery__item"><img class="gallery__image" data-atl = "${image.description}" data-link = "${image.original}" src="${image.preview}" alt="${image.description}" width="719" height="480"/></li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", markup);
 
-console.log(galleryItems);
+
+
+const enhanceImg = (event) => {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const selectedImg = event.target.dataset.link;
+  const alternativeText = event.target.dataset.alt;
+  gallery.insertAdjacentHTML("afterend", `<div class="modal"><img class="modal__image" src="${selectedImg}" alt="${alternativeText}" width="1280" height="853"/></div>`);
+};
+
+gallery.addEventListener("click", enhanceImg)
