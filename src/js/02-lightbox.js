a28b0e39a0196645bbd8be09b53cf37a8d15664b
Oskar Przybysz <117311648+ProgramistaWellness@users.noreply.github.com>
2023-05-18 02:17:49 +0200

    added options to lightbox

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    (image) =>
+      `<li>
+      <a class="gallery__item" href="${image.original}">
+        <img
+          class="gallery__image"
+          src="${image.preview}"
+          alt="${image.description}"
+        />
+      </a>
+    </li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", markup);
 console.log(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', { captionData: "alt", captionDelay: 250, });
